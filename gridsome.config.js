116716b5a01cfd2b3d6c88a34839bdd9ad566f33
Jason Dittmer <jason@zrimarketing.com>
2020-07-29 13:08:18 -0500

    Changed site name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'Body Art Pros',
+  siteDescription: ' Private Tattoo & Piercing Shop in Neosho, Missouri',
 
   templates: {
     Post: '/:title',
